10dfcccd2cb5e5c501fc573372b9082300de7b0a
h-chayma <chaymafarhat16@gmail.com>
2024-11-04 13:44:20 +0100

    Add analytics

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+// @ts-ignore
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true }
+	}),
 	integrations: [
 		starlight({
 			title: 'El Aroui Stories',
